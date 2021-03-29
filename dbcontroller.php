a79830f97f31d570892c026bf9cc40b5478f5577
Tyler Pranger <tylerpranger@gmail.com>
2021-03-29 13:03:04 -0600

    updated php to use mysqli instead of pdo

--- dbcontroller.php
+++ dbcontroller.php
@@ -2,22 +2,30 @@
 
 class Dbcontroller{
     private $host = "localhost";
-    private $dbname = "training_program";
-    private $user = "tylerpranger";
-    private $password = "";
-    private $conn = NULL;
+    private $dbname = "Training";
+    private $user = "root";
+    private $pwd = "root";
+    // private $conn = NULL;
 
     function connect(){
-        $this->conn = new PDO("pgsql:host=".$this->host.";dbname=".$this->dbname, $this->user, $this->password);
-        // $conn = new PDO('pgsql:host=localhost;dbname=training_program', 'tylerpranger','');
-        if($this->conn === false){
-            die("ERROR: Could not connect.");
+
+        //MySQL DB Connection
+        $conn = mysqli_connect($this->host, $this->user, $this->pwd, $this->dbName);
+        if($conn === false){
+            die("ERROR: Could not connect. " . mysqli_connect_error());
         }
-        return $this->conn;
+        return $conn;
+
+        //postgreSQL attempt
+        // $this->conn = new PDO("pgsql:host=".$this->host.";dbname=".$this->dbname, $this->user, $this->password);
+        // // $conn = new PDO('pgsql:host=localhost;dbname=training_program', 'tylerpranger','');
+        // if($this->conn === false){
+        //     die("ERROR: Could not connect.");
+        // }
     }
 
     function close_connection(){
-        $this->conn = NULL;
+        // $this->conn = NULL;
     }
     // $this->conn = new PDO('pgsql:host=localhost;dbname=training_program', 'tylerpranger',''); 
 }
--- home.php
+++ home.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+
 <?php
     require_once("dbcontroller.php");
 
@@ -16,12 +18,13 @@
 <head>
 </head>
 <body>
-    <form method="get">
+    
+    <!-- <form method="get">
         <label for="fname">First name:</label>
         <input type="text" id="fname" name="fname"><br><br>
         <label for="lname">Last name:</label>
         <input type="text" id="lname" name="lname"><br><br>
         <input type="submit" value="Submit">
-    </form>
+    </form> -->
 </body>
 </html>
