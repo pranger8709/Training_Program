16d5f8bb06e577ce33bcf033b6d7eb1bb6369063
Tyler Pranger <tylerpranger@gmail.com>
2021-04-01 23:55:10 -0600

    updated account page with modal login and register views

--- account.php
+++ account.php
@@ -26,6 +26,60 @@
         </div>
     </header>
     <div class="main_body">
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+        <span class="close" onclick="close_modal_login();">&times;</span>
+            <div id="login_page" class="login_page">
+                <form id="login_form" method="POST" style="margin: auto;display: flex;">
+                <table style="margin: auto;">
+                    <tr>
+                        <td><h1 style="color:white;">Login</h1></td>
+                    </tr>
+                    <tr>
+                        <td><input placeholder="Username" class="account_form_buttons"/></td>
+                    </tr>
+                    <tr>
+                        <td><input placeholder="Password" class="account_form_buttons"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" class="submit_button account_form_buttons"/></td>
+                    </tr>
+                </table>
+                </form>
+            </div>
+            <div id="register_page" class="login_page">
+                <form id="login_form" method="POST" style="margin: auto;display: flex;">
+                <table style="margin: auto;">
+                    <tr>
+                        <td><h1 style="color:white;">Register</h1></td>
+                    </tr>
+                    <tr>
+                        <td><input placeholder="First Name" class="account_form_buttons"/></td>
+                    </tr>
+                    <tr>
+                        <td><input placeholder="Last Name" class="account_form_buttons"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="email" placeholder="Email" class="account_form_buttons"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="password" placeholder="Password" class="account_form_buttons"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="password" placeholder="Confirm Password" class="account_form_buttons"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" class="submit_button account_form_buttons"/></td>
+                    </tr>
+                </table>
+                </form>
+            </div>
+        </div>
+
+        </div>
         <div id="menu" onmouseleave="mouse_toggle_menu();">
             <a href="home.php"><div class="menu_item">Home</div></a>
             <a href="account.php"><div class="menu_item">Login/Register</div></a>
@@ -35,10 +89,10 @@
         </p>
         <table style="height:50%;width:100%;vertical-align:middle;position:fixed">
             <tr>
-                <td><button class="account_buttons">Login</button></td>
+                <td><button id="login" class="account_buttons" onclick="open_modal_login();">Login</button></td>
             </tr>
             <tr>
-                <td><button class="account_buttons">Register</button></td>
+                <td><button id="register" class="account_buttons" onclick="open_modal_register();">Register</button></td>
             </tr>
         </table> 
     </div>
