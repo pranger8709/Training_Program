c1a82a740bb5d7793d8164d760a285dad4e0ec7a
Tyler Pranger <tylerpranger@gmail.com>
2021-04-07 23:56:30 -0600

    trying pdo

--- account.php
+++ account.php
@@ -12,10 +12,16 @@
         // echo $username;
         // $sql = "SELECT users.email, users.password FROM users WHERE users.email = '".$_POST["username"]."'";
         $sql = "SELECT * FROM training.users WHERE first_name='Tony'";
-        echo $sql."<br>";
+        // $sql = 'SELECT name, color, calories FROM fruit ORDER BY name';
+        foreach ($conn->query($sql) as $row) {
+            print $row['first_name'] . "\t";
+            // print $row['color'] . "\t";
+            // print $row['calories'] . "\n";
+        }
+        // echo $sql."<br>";
         // $result = mysqli_query($conn, $sql);
-        $result = $conn->query($sql);
-        printf("Select returned %d rows.\n", $result->num_rows);
+        // $result = $conn->query($sql);
+        // printf("Select returned %d rows.\n", $result->num_rows);
         // if (mysqli_num_rows($result) > 0) {
         // // output data of each row
         //     while($row = mysqli_fetch_assoc($result)) {
--- dbcontroller.php
+++ dbcontroller.php
@@ -11,11 +11,16 @@ class Dbcontroller{
         //MySQL DB Connection $_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']
         // $conn = new mysqli($_SERVER[$this->host], $_SERVER[$this->user], $_SERVER[$this->pwd], $_SERVER[$this->dbName], $_SERVER[3306]);
         // $conn = mysqli_connect($this->host, $this->user, $this->pwd, $this->dbName, 3306);
-        $conn = new mysqli($this->host, $this->user, $this->pwd, $this->dbName, 3306);
-        $conn->set_charset("utf8");
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
+        try{
+            $conn = new PDO('mysql:host=capstone.cubp1kd6anhq.us-east-1.rds.amazonaws.com;port=3306;dbname=training', 'admin', 'Unicorn2009!');
+        }catch (PDOException $pe) {
+            die("Could not connect to the database $dbname :" . $pe->getMessage());
         }
+        // $conn = new mysqli($this->host, $this->user, $this->pwd, $this->dbName, 3306);
+        // $conn->set_charset("utf8");
+        // if ($conn->connect_error) {
+        //     die("Connection failed: " . $conn->connect_error);
+        // }
         // if($conn === false){
         //     die("ERROR: Could not connect. " . mysqli_connect_error());
         // }
