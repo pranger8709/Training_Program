4ad121722b2488c8a6d90ca0cda0a0a316f67f14
Tyler Pranger <tylerpranger@gmail.com>
2021-04-07 21:44:41 -0600

    hopefully this will work with the database

--- dbcontroller.php
+++ dbcontroller.php
@@ -8,8 +8,8 @@ class Dbcontroller{
 
     protected function connect(){
 
-        //MySQL DB Connection
-        $conn = mysqli_connect($this->host, $this->user, $this->pwd, $this->dbName);
+        //MySQL DB Connection $_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']
+        $conn = new mysqli($_SERVER[$this->host], $_SERVER[$this->user], $_SERVER[$this->pwd], $_SERVER[$this->dbName], $_SERVER[3306]);
         if($conn === false){
             die("ERROR: Could not connect. " . mysqli_connect_error());
         }
