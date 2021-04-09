7c14ea77af771b4337611b5047083579cc3e2ed0
Tyler Pranger <tylerpranger@gmail.com>
2021-04-08 23:12:19 -0600

    added account_settings, exercise_stats, generage_workout pages and to the menu

--- exercise_stats.php
+++ exercise_stats.php
@@ -12,7 +12,7 @@
 
 <html>
 <head>
-    <title>Training</title>
+    <title>Exercise Stats</title>
     <link rel="stylesheet" type="text/css" href="Style/theme.css" />
     <script src="main.js"></script>
 </head>
@@ -22,7 +22,7 @@
             <div class="menu_icon_container">
                 <img class="menu_icon" src="Style/Images/menu.svg" onclick="toggle_menu();"/>
             </div>
-            <h1 class="title">Training Program</h1>
+            <h1 class="title">Exercise Stats</h1>
             <div class="menu_icon_container">
                 <img class="logo" src="Style/Images/logo.png"/>
             </div>
@@ -43,7 +43,7 @@
             }
             ?>
         </div>
-        <h3>Welcome to the Workout Training Program!</h3>
+        <!-- <h3>Welcome to the Workout Training Program!</h3>
         <table class="main_page_table">
             <tr>
                 <td>&ensp;&ensp;This is the place for those self-motivated individuals to want to train themseleves to be the best version of you. Whether you are just starting out on your journey or if you already track your progress another way we can help either way. To get started is free and really quite simple, create an account using you email address. Once you are regisitered you are able to generate a custom workout, if you do not like the workout generated you can either generate a new one or customize the current workout generated.</td>
@@ -51,7 +51,7 @@
             <tr>
                 <td><img class="home_page_image" src="Style/Images/home-1.png"/></td>
             </tr>
-        </table>
+        </table> -->
     </div>
 </body>
 </html>
