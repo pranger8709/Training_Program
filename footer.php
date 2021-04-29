b75477d2c0fe582a40d028c3db8030f13d94fe9c
Tyler Pranger <tylerpranger@gmail.com>
2021-04-28 20:14:00 -0600

    finished footer on all pages

--- footer.php
+++ footer.php
@@ -1,18 +1,16 @@
 <footer class="footer">
-    <!-- <div class="header_container">
-        <div class="menu_icon_container">
-            <img class="menu_icon" src="Style/Images/menu.svg" onclick="toggle_menu();"/>
-            <?php
-            //If the user is logged in then it will display the first name of the user, otherwise nothing will happen
-                $first_name = $_SESSION['first_name'] ?? null;
-                if($first_name != null){
-                    echo "<div style=\"padding-left: 5%;\">Hello, ".$_SESSION['first_name']."</div>";
-                } 
-            ?>
-        </div>
-        <h2 class="title">Home</h2>
-        <div class="logo_icon_container">
-            <a href="home.php"><img class="logo" src="Style/Images/logo.png"/></a>
-        </div>
-    </div>  -->
+    <div class="about_us_container">
+        Contact us
+        <br>
+        Email: support@fitnessprogram.com
+        <br>
+        Phone Number: (801) 555-0101
+    </div>
+    <div class="author_container">
+        Author
+        <br>
+        Tyler Pranger
+        <br>
+        Master of Software Development Capstone Project
+    </div>
 </footer>
