ca2d86e903d4bdf094c1ba71ed9e11d298de48dc
Tyler Pranger <tylerpranger@gmail.com>
2021-04-28 22:44:54 -0600

    fixed footer anchor text color and added mailto for email address in footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="about_us_container">
         Contact us
         <br>
-        Email: support@fitnessprogram.com
+        Email: <a href="mailto:support@fitnessprogram.com">support@fitnessprogram.com</a>
         <br>
         Phone Number: (801) 555-0101
     </div>
